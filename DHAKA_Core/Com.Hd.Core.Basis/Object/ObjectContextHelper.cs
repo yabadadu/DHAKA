7f733c68925f91bdcb3f35a044376d60c87608ba
yabadadu <88810408+yabadadu@users.noreply.github.com>
2021-12-22 12:36:48 +0900

    Add files via upload

---
+++ DHAKA_Core/Com.Hd.Core.Basis/Object/ObjectContextHelper.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace Com.Hd.Core.Basis.Object
+{
+    public static class ObjectContextHelper
+    {
+        public static T CreateInstance<T>(string typeName)
+        {
+            typeName = typeName.Trim();
+            var type = Type.GetType(typeName);
+            var obj = (T) Activator.CreateInstance(type);
+            return obj;
+        }
+
+        public static T CreateInstance<T>(string typeName, string assemblyName)
+        {
+            if (string.IsNullOrEmpty(assemblyName) == false)
+            {
+                typeName = typeName + "," + assemblyName;
+            }
+            return CreateInstance<T>(typeName);
+        }
+    }
+}
