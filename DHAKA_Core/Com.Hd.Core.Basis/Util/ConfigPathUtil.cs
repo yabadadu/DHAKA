8b0db211781f1d0cbf472986f4f8206ce5734ab1
yabadadu <88810408+yabadadu@users.noreply.github.com>
2021-12-22 12:34:59 +0900

    Add files via upload

---
+++ DHAKA_Core/Com.Hd.Core.Basis/Util/ConfigPathUtil.cs
@@ -0,0 +1,60 @@
+﻿#region
+
+using Com.Hd.Core.Basis.Config;
+using Com.Hd.Core.Basis.Constant;
+
+#endregion
+
+namespace Com.Hd.Core.Basis.Util
+{
+    public class ConfigPathUtil
+    {
+        public static string GetConfigPath(ConfigType configType, string configName)
+        {
+            var rtnValue = string.Empty;
+            string rootPath;
+            AppConfigurationManager appCfgManager;
+            string prmDir;
+            string envPath;
+            string exeCode;
+
+            switch (configType)
+            {
+                case ConfigType.AppConfig:
+                    var exeFilePath = PathUtil.GetMyExeFilePath();
+                    rtnValue = PathUtil.AppendPath(exeFilePath, ConfigConstant.FILE_EXT_APPCONFIG);
+                    break;
+                case ConfigType.Environment:
+                    rootPath = PathUtil.GetRootPath();
+
+                    appCfgManager = ConfigManagerContext.GetAppConfigurationManager();
+                    prmDir = appCfgManager.GetConfig(AppConfigConstant.PROGRAM_DIR_CONF);
+                    exeCode = appCfgManager.GetConfig(AppConfigConstant.PROGRAM_ID);
+                    envPath = appCfgManager.GetConfig(AppConfigConstant.PATH_ENVIRONMENT);
+                    
+                    rtnValue = PathUtil.AppendPath(rootPath, prmDir, exeCode, envPath, configName);
+                    break;
+                case ConfigType.Resource:
+                    rootPath = PathUtil.GetRootPath();
+
+                    appCfgManager = ConfigManagerContext.GetAppConfigurationManager();
+                    prmDir = appCfgManager.GetConfig(AppConfigConstant.PROGRAM_DIR_CONF);
+                    exeCode = appCfgManager.GetConfig(AppConfigConstant.PROGRAM_ID);
+                    envPath = appCfgManager.GetConfig(AppConfigConstant.PATH_RESOURCE);
+
+                    rtnValue = PathUtil.AppendPath(rootPath, prmDir, exeCode, envPath, configName);
+                    break;
+                case ConfigType.Report:
+                    break;
+                case ConfigType.Log:
+                    break;
+                default:
+                    break;
+            }
+
+            return rtnValue;
+        }
+
+
+    }
+}
---
+++ DHAKA_Core/Com.Hd.Core.Basis/Util/PathUtil.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using System;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Com.Hd.Core.Basis.Util
+{
+    public class PathUtil
+    {
+        public static string GetFilePath(string fileName)
+        {
+            var isPathRooted = Path.IsPathRooted(fileName);
+            return isPathRooted ? fileName : AppendPath(GetRootPath(), fileName);
+        }
+
+        public static string GetRootPath()
+        {
+            return AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        public static string AppendPath(params string[] args)
+        {
+            return args.Aggregate(string.Empty, Path.Combine);
+        }
+
+        public static string GetMyExeFilePath()
+        {
+            var rtnValue = System.Reflection.Assembly.GetEntryAssembly().Location;
+            return rtnValue;
+        }
+    }
+}
